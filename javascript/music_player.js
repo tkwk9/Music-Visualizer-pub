6d4438507a09de2a88d9e3cb36d000783227c3e1
Tim Kwak <tim.k.kwak@gmail.com>
2018-01-18 09:38:01 -0500

    Refine equalizer

--- javascript/music_player.js
+++ javascript/music_player.js
@@ -6,51 +6,41 @@ class MusicPlayer {
     this.flippedMode['play'] = 'paused';
 
     this.audio = $(".audio-source")[0];
-    // this.audio.crossOrigin = "anonymous";
-
     this.ctx = new (AudioContext || window.webkitAudioContext)();
     this.analyser = this.ctx.createAnalyser();
 
     this.analyser.fftSize = 2048;
     this.audioSrc = this.ctx.createMediaElementSource(this.audio);
-    this.freqArray = new Uint8Array(this.analyser.frequencyBinCount);
-    console.log(this.freqArray.length);
+    this.freqArray = new Float32Array(this.analyser.frequencyBinCount);
+
     this.audioSrc.connect(this.analyser);
     this.audioSrc.connect(this.ctx.destination);
+
+    this.barCount = 128;
+    this.heightMultiplier = 3;
 
     this.addListeners();
     this.addBars();
 
   }
 
-  addBars() {
+  addBars() { // This is Test Function, this will move somewhere else
     let tempArray = [];
-    for (let i = 0; i < 64; i++){
+    for (let i = 0; i < this.barCount ; i++){
       tempArray.push($(`<div class="testDivs ${i}"></div>`));
     }
-    // this.shuffleArray(tempArray);
     tempArray.forEach(div => {
       $(".test").append(div);
     });
   }
 
   letThereBe() {
-    this.analyser.getByteFrequencyData(this.freqArray);
-    // this.analyser.getByteTimeDomainData(this.timeArray);
-    this.processFreqArray().forEach((val, ind) => {
-      // console.log(ind);
-      // let newVal = Math.pow(val, Math.floor(((this.analyser.frequencyBinCount - ind)/this.analyser.frequencyBinCount) * 10)+1)/Math.pow(255, Math.floor(((this.analyser.frequencyBinCount - ind)/this.analyser.frequencyBinCount) * 10))* 10;
-      // let newVal1 = ((val - (63-ind)*2)/(255 - (63-ind)*2))*255;
-      let newVal;
-      // newVal = Math.pow(val, (((64 - ind)/64) * 5)+1)/
-      // Math.pow(255, (((64 - ind)/64) * 5));
-      // if (ind < 15){
-      // } else{
-      //   newVal = (Math.pow(val/255, (1/((ind + 1)/64))) * 255);
-      // }
-      $(`.testDivs.${ind}`).height(val + 10);
-    });
-    this.timeOutId = setTimeout(this.letThereBe.bind(this), 16);
+    this.analyser.getFloatFrequencyData(this.freqArray);
+    this.processFreqArray().forEach( (height, ind) => {
+        $(`.testDivs.${ind}`).height(height);
+      }
+    );
+    this.timeoutId = setTimeout(this.letThereBe.bind(this), 16);
   }
 
   shuffleArray(array) {
@@ -63,18 +53,16 @@ class MusicPlayer {
   }
 
   processFreqArray() {
-    let tempArray = new Array(64);
-    for (let i = 0; i < 64; i++){
-      tempArray[i] = this.freqArray[i * 11 + 16];
-      // console.log(i * 11 + 16);
+
+    let tempArray = [];
+
+    for (let i = 0; i<this.barCount ; i++) {
+      let val = this.freqArray[i] + 140;
+      let curveIntensity = (this.barCount - 1 - i) * (3/127) + 1;
+      val = Math.pow(val, curveIntensity + 1)/Math.pow(140, curveIntensity) * this.heightMultiplier;
+      tempArray.push(val);
     }
-    // let adder = this.freqArray.slice(790).reduce((acc, cum) => {
-    //   return acc + cum;
-    // }, 0);
-    // for (let i = 59; i < 64; i++){
-    //   tempArray[i] += adder;
-    // }
-    // return tempArray.map( val => Math.floor(val/32));
+
     return tempArray;
   }
 
@@ -88,15 +76,13 @@ class MusicPlayer {
         this.mode = this.flippedMode[this.mode];
         this.audio.pause();
         $(".play-button img").attr("src", "images/circular-play-button.svg");
-        clearTimeout(this.timeOutId);
+        clearTimeout(this.timeoutId);
         break;
       case 'paused':
         this.mode = this.flippedMode[this.mode];
         this.audio.play();
         $(".play-button img").attr("src", "images/circular-pause-button.svg");
-        // this.intervalId = setInterval(this.letThereBe.bind(this), 16);
-        this.timeOutId = setTimeout(this.letThereBe.bind(this), 16);
-
+        this.timeoutId = setTimeout(this.letThereBe.bind(this), 16); // timeout enables Soundbar Visuals
         break;
     }
   }
